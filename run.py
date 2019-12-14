250ecc9b775925de71416633707c75c9ec2fc509
Kebasoh <ongatikebaso@gmail.com>
2019-12-14 20:13:34 +0300

    added the save def at the run.p

--- run.py
+++ run.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3.6
+from user import User
+
+def __init__(account_name,user_name,password,email):
+   
+    new_user = User(account_name,user_name,password,email)
+    return new_user
+def save_user(user):
+   
+    user.save_user()
